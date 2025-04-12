0c24009c34018ac91927d22846f5c08dd9862969
seoyoungbyun <qustjdud2181@gmail.com>
2025-04-13 03:48:39 +0900

    [feat]: [#4]커뮤니티 DTO 및 DAO interface 구현

---
+++ src/main/java/spring/tripmate/dto/PostResponseDTO.java
@@ -0,0 +1,79 @@
+package spring.tripmate.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.util.List;
+import java.util.Map;
+
+public class PostResponseDTO {
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class PostDTO{
+        @NotNull
+        private Long postId;
+        @NotBlank
+        private String title;
+
+        private List<String> images;
+        @NotBlank
+        private String content;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class UpdateDTO{
+        private Map<String, Object> updatedFields;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class SummaryDTO{
+        private List<SummaryPostDTO> posts;
+
+        @Getter
+        @Setter
+        public static class SummaryPostDTO{
+            @NotNull
+            private Long postId;
+            @NotBlank
+            private String nickname;
+
+            private String profile;
+            @NotBlank
+            private String title;
+
+            private List<String> images;
+            @NotBlank
+            private String content;
+        }
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class DetailDTO{
+        @NotNull
+        private Long postId;
+        @NotBlank
+        private String nickname;
+
+        private String profile;
+        @NotBlank
+        private String title;
+
+        private List<String> images;
+        @NotBlank
+        private String content;
+        @NotNull
+        private List<PlanResponseDTO.PlanDTO> plan;
+    }
+}
