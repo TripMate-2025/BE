9d3a47d00c77d666edbd2b7a075e37b6d8ac87a4
minju <iaki111@naver.com>
2025-04-28 23:40:07 +0900

    [fix]: ConsumerDAO

--- src/main/java/spring/tripmate/dao/ConsumerDAO.java
+++ src/main/java/spring/tripmate/dao/ConsumerDAO.java
@@ -1,10 +1,11 @@
 package spring.tripmate.dao;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import spring.tripmate.domain.Consumer;
 
-public interface ConsumerDAO {
+public interface ConsumerDAO extends JpaRepository<Consumer, Long>{
 	
     Consumer findByEmail(String email);
     
