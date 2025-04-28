6f1a9ec88aa7f7f3b8073d20e6af067cc13a9309
seoyoungbyun <qustjdud2181@gmail.com>
2025-04-29 00:01:52 +0900

    [fix]: [#3]TravelRoom dao 수정

--- src/main/java/spring/tripmate/dao/RoomMemberDAO.java
+++ src/main/java/spring/tripmate/dao/RoomMemberDAO.java
@@ -3,15 +3,19 @@ package spring.tripmate.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
+import spring.tripmate.domain.Consumer;
 import spring.tripmate.domain.mapping.RoomMember;
 
 import java.util.List;
 
 public interface RoomMemberDAO extends JpaRepository<RoomMember, Long> {
 
-    List<RoomMember> findByConsumerId(Long consumerId);
+    RoomMember findByConsumerId(Long consumerId);
 
     List<RoomMember> findByRoomId(Long roomId);
+
+    @Query("SELECT rm.member FROM RoomMember rm WHERE rm.room.id = :roomId")
+    List<Consumer> findConsumersByRoomId(Long roomId);
 
     @Transactional
     void deleteByConsumerIdAndRoomId(Long consumerId, Long roomId);
--- src/main/java/spring/tripmate/dao/TravelRoomDAO.java
+++ src/main/java/spring/tripmate/dao/TravelRoomDAO.java
@@ -1,25 +1,9 @@
 package spring.tripmate.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.transaction.annotation.Transactional;
-import spring.tripmate.domain.mapping.RoomMember;
+import spring.tripmate.domain.TravelRoom;
 
-import java.util.List;
-import java.util.Optional;
-
-public interface TravelRoomDAO extends JpaRepository<RoomMember, Long> {
-
-    Optional<RoomMember> findById(Long memberId);
-
-    List<RoomMember> findByConsumerId(Long consumerId);
-
-    List<RoomMember> findByRoomId(Long roomId);
-
-    @Transactional
-    void deleteByConsumerIdAndRoomId(Long consumerId, Long roomId);
-
-    @Query("SELECT CASE WHEN COUNT(rm) > 0 THEN true ELSE false END FROM RoomMember rm WHERE rm.room.id = :roomId AND rm.member.id = :consumerId")
-    boolean existsByRoomIdAndConsumerId(Long roomId, Long consumerId);
+public interface TravelRoomDAO extends JpaRepository<TravelRoom, Long> {
+    TravelRoom findByPlanId(Long planId);
 }
 
