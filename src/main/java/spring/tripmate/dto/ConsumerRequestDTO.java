1d517973f216d075bd0446c30045dc6b8cc1a68c
minju <iaki111@naver.com>
2025-04-28 21:18:50 +0900

    [add]: consumer DTO 추가

--- src/main/java/spring/tripmate/dto/ConsumerRequestDTO.java
+++ src/main/java/spring/tripmate/dto/ConsumerRequestDTO.java
@@ -1,5 +1,41 @@
 package spring.tripmate.dto;
 
-public class ConsumerRequestDTO {
+import jakarta.validation.constraints.*;
+import lombok.*;
 
+public class ConsumerRequestDTO {
+	@Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class RegisterDTO {
+        @NotBlank
+        private String username;
+        @NotBlank @Email
+        private String email;
+        @NotBlank
+        private String password;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class LoginDTO {
+        @NotBlank @Email
+        private String email;
+        @NotBlank
+        private String password;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class UpdateDTO {
+        private String username;
+        @Email
+        private String email;
+        private String password;
+    }
 }
